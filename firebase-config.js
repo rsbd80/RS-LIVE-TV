42642ff1b78e3952341febd59f590622bb4e9876
Tv-bd2 <rspremium64@gmail.com>
2026-06-11 07:29:38 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase কনফিগারেশন - আপনার ডাটাবেসের সাথে সংযোগ করবে
 const firebaseConfig = {
     apiKey: "AIzaSyBW7EtBSaGq4BO4QuSy2unTJrMobQ3lIfU",
     authDomain: "rs-tv-admin.firebaseapp.com",
@@ -8,7 +7,6 @@ const firebaseConfig = {
     appId: "1:619060422153:web:610a287d0a0dae0299fad8"
 };
 
-// Firebase Initialize
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
